8e413ed774163ae64fc6fbf8a17cacc3765f52cd
hxili <hxili@ucdavis.edu>
2023-12-11 09:07:28 -0800

    update

--- Script/filter.py
+++ Script/filter.py
@@ -7,28 +7,38 @@ parser.add_argument('-o', type=str, metavar='<path>', help='output file')
 arg = parser.parse_args()
 
 o = open(arg.o, 'w')
-# get the gene ensembl id list
-ids = []
+# get the gene ensembl id list and symbol list
+ENSGs = []
+syms = []
 
 with open(arg.f1) as f:
 	while True:
 
 		line = f.readline()
 		if line == '': break
-		id = line.split(',')[1].strip("\"")
-
-		if id != 'None': ids += [id]
+		ENSG = line.split(',')[1].strip("\"")
+		sym = line.split(',')[0].strip("\"")
+		if ENSG != 'None': 
+			ENSGs += [ENSG]
+			syms += [sym]
 
 	# keep header in output file
-	ids = ['Name'] + ids[1:]
+	ENSGs = ENSGs[1:]
 
+n=1
 # only rows needed remain
 with open(arg.f2) as f:
 	while True:
+		n+=1
+		if n==6: break
 		line = f.readline()
+		print(line[:30])
 		if line == '': break
-		id = line.split('	')[0].split('.')[0].strip("\"")
-		if id in ids:
+		ENSG = line.split('	')[0].split('.')[0]
+		if ENSG == 'Name': o.write(line)
+		if ENSG in ENSGs:
+#			newline = line.split('	')
+#			newline[1] = 
 			o.write(line)
 #			ids.remove(id)
 
